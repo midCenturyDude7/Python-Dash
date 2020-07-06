d73aafa8a47963423b7376e196bc30b2b681b31b
midCenturyDude7 <mgriffes@gmail.com>
2020-07-05 21:48:18 -0700

    Working with connection to sqlite3 in Python file

--- dash_venv/index.py
+++ dash_venv/index.py
@@ -3,19 +3,42 @@ Filename: index.py
 Initial practice with Dash
 """
 
-import dash
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import dash_table
+# Import dependencies
+import numpy as np
 import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as tfhub
 import sqlite3
+from sqlite3 import Error
+import io
 
-import plotly.graph_objs as go
-
-conn = sqlite3.connect(r"C:\Users\MTGro\Desktop\coding\wineApp\db\wine_data.sqlite")
+# Create a connection to the sqlite database
+conn = sqlite3.connect('db\wine_data.sqlite', detect_types = sqlite3.PARSE_DECELTYPES)
 c = conn.cursor()
-df = pd.read_sql("select * from wine_data", conn)
-df = df[['country', 'description', 'rating', 'price','province','title','variety','winery','color']]
-df.head(1)
+
+# Read the table in the database
+wine_df = pd.read_sql('Select * from wine_data', conn)
+
+# Drop the duplicate descriptions
+wine_df = wine_df.drop_duplicates('description')
+
+# Drop null prices
+wine_df = wine_df.dropna(subset=['price'])
+
+# Filter the dataframe to include only varieties with more than 200 reviews
+wine_df = wine_df.groupby('variety').filter(lambda x: len(x) > 200)
+
+# Create a column named color
+wine_df["color"] = ""
+
+# Used to update the database with the wine color. Manually updated each wine variety
+c.execute("update wine_data set color = 'red' where variety = 'Aglianico'  ")
+
+# Commit the update to the database so it saves
+conn.commit()
+
+# Remove all the records without a color
+wine_df = pd.read_sql("select country, description,rating,price,province,title,variety, winery, color  from wine_data where color in ('red', 'white', 'other')", conn)
+wine_df.to_sql('wine_data', conn, if_exists = "replace")
+
+print(wine_df.head())
