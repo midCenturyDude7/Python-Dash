23aeef7a346e0d1abbd104b98e6b40863c1037cb
midCenturyDude7 <mgriffes@gmail.com>
2020-07-06 21:30:18 -0700

    Table generator for reuse with Dash

---
+++ table_generator.py
@@ -0,0 +1,40 @@
+"""
+Filename = table_generator.py
+Working with reusable components in Dash by building a template for a table
+"""
+
+# Dependencies
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+# Read in the data and convert to a dataframe via Pandas
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+
+# Generate the tabular data
+def generate_table(dataframe, max_rows=25):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
